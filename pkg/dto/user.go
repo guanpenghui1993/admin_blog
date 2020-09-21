8f859bf9fb7c9825b56edfa1eff70d769834f029
guanpenghui <guanpenghui@qianxin.com>
2020-09-21 17:46:12 +0800

    xxx

--- pkg/dto/user.go
+++ pkg/dto/user.go
@@ -1,12 +1,6 @@
 package dto
 
-import "github.com/asaskevich/govalidator"
-
 type UserLoginDto struct {
-	Username string `valid:"required,length(8|20)"`
-	Password string `valid:"required,length(8|20)"`
-}
-
-func demo() {
-	govalidator.IsURL(`http://user@pass:domain.com/path/page`)
+	Username string `binding:"required"`
+	Password string `binding:"required"`
 }
